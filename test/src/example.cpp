61a4c2db54e50966e1f0ffb3849ee60c4670e114
Mark Busenitz <mbusenitz@gmail.com>
2026-02-23 14:00:22 -0800

    move 3rdparty lib, fix build and crash (#2)
    Co-authored-by: mucbuc <mucbuc@mucbucs-Mac-mini-2.local>

--- test/src/example.cpp
+++ test/src/example.cpp
@@ -6,21 +6,22 @@
 int main()
 {
     ftdcmp::init();
+    {
+    #ifdef __EMSCRIPTEN__
+        auto dcmp = ftdcmp::make_decomposer_l("ArialUnicode.ttf");
+    #else
+        auto dcmp = ftdcmp::make_decomposer_l("/Library/Fonts/Arial Unicode.ttf");
+    #endif
 
-#ifdef __EMSCRIPTEN__
-    auto dcmp = ftdcmp::make_decomposer_l("ArialUnicode.ttf");
-#else
-    auto dcmp = ftdcmp::make_decomposer_l("/Library/Fonts/Arial Unicode.ttf");
-#endif
+        const auto a = dcmp('a');
+        std::cout << path_comp::make_html_path(a, "container") << std::endl;
 
-    const auto a = dcmp('a');
-    std::cout << path_comp::make_html_path(a, "container") << std::endl;
+        std::cout << a.loops()[0].max()[0] << " " << a.loops()[0].max()[1] << std::endl;
+        std::cout << a.loops()[0].min()[0] << " " << a.loops()[0].min()[1] << std::endl;
 
-    std::cout << a.loops()[0].max()[0] << " " << a.loops()[0].max()[1] << std::endl;
-    std::cout << a.loops()[0].min()[0] << " " << a.loops()[0].min()[1] << std::endl;
-
-    std::cout << path_comp::make_html_path(dcmp('B'), "container2") << std::endl;
-
+        std::cout << path_comp::make_html_path(dcmp('B'), "container2") << std::endl;
+    }
+    
     ftdcmp::release();
 
     return 0;
