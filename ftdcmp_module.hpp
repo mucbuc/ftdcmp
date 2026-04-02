18e9eaa78dec0e39eeeb8d7a125c8e68a12d9c82
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2026-04-02 08:42:56 -0700

    module improvements

--- ftdcmp_module.hpp
+++ ftdcmp_module.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
+#include "ftdcmp.hpp"
+
 namespace ftdcmp {
 template <typename Base>
 struct Module : public Base {
 
     template <typename... U>
-    Module(U... init)
-        : Base(init...)
+    Module(U&&... init)
+        : Base(std::forward<U>(init)...)
     {
     }
+
+    Module(Module&&) = default;
 
     void init(auto done)
     {
